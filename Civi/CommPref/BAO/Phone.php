08cc7b184cfbefd49bf5810c37df1ca12e7d83ec
Kurund Jalmi <kurundjalmi@thirdsectordesign.org>
2022-01-10 19:26:04 +0000

    more work on phone processing

---
+++ Civi/CommPref/BAO/Phone.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Civi\CommPref\BAO;
+
+class Phone {
+
+  /**
+   * Function to save the phone number
+   *
+   * @param int $contactId
+   * @param array $params
+   *
+   * @return void
+   */
+  public static function processPhone($contactId, $params) {
+    // submitted values
+    $phoneMobile = $params[0]['phone_mobile'];
+    $phoneLandline = $params[0]['phone_landline'];
+
+    // get phone information
+    [$mobileNumber, $landlineNumber] = self::getPhone($contactId);
+
+    // update or delete phone numbers
+    // mobile
+    if (empty($phoneMobile) && !empty($mobileNumber)) {
+      self::deletePhone($contactId, 'Mobile');
+    }
+    elseif ($phoneMobile != $mobileNumber) {
+      self::updatePhone($contactId, $phoneMobile, 'Mobile');
+    }
+
+    // landline
+    if (empty($phoneLandline) && !empty($landlineNumber)) {
+      self::deletePhone($contactId, 'Phone');
+    }
+    elseif ($phoneLandline != $landlineNumber) {
+      self::updatePhone($contactId, $phoneLandline, 'Phone');
+    }
+  }
+
+  /**
+   * Function to get phone numbers
+   *
+   * @param int $contactId
+   *
+   * @return array
+   */
+  public static function getPhone($contactId) {
+    $phones = \Civi\Api4\Phone::get(FALSE)
+      ->addSelect('phone', 'phone_type_id:name')
+      ->addWhere('contact_id', '=', $contactId)
+      ->addWhere('phone_type_id', 'IN', [1, 2])
+      ->execute();
+
+    // get phone numbers
+    $mobileNumber = '';
+    $landlineNumber = '';
+    foreach ($phones as $phone) {
+      if ($phone['phone_type_id:name'] == 'Mobile') {
+        $mobileNumber = $phone['phone'];
+      }
+      elseif ($phone['phone_type_id:name'] == 'Phone') {
+        $landlineNumber = $phone['phone'];
+      }
+    }
+
+    return [$mobileNumber, $landlineNumber];
+  }
+
+  /**
+   * Function to update the phone number
+   *
+   * @param int $contactId
+   * @param string $phone
+   * @param string $phoneType
+   */
+  public static function updatePhone($contactId, $phone, $phoneType) {
+    \Civi\Api4\Phone::update(FALSE)
+      ->addWhere('contact_id', '=', $contactId)
+      ->addWhere('phone_type_id:name', '=', $phoneType)
+      ->setValues(['phone' => $phone])
+      ->execute();
+  }
+
+  /**
+   * Function to delete the phone number
+   *
+   * @param int $contactId
+   * @param string $phoneType
+   */
+  public static function deletePhone($contactId, $phoneType) {
+    \Civi\Api4\Phone::delete()
+      ->addWhere('contact_id', '=', $contactId)
+      ->addWhere('phone_type_id:name', '=', $phoneType)
+      ->execute();
+  }
+
+}
--- Civi/CommPref/Form/Submit.php
+++ Civi/CommPref/Form/Submit.php
@@ -5,8 +5,21 @@ namespace Civi\CommPref\Form;
 class Submit {
 
   public function process($event) {
-    // print_r($event);
-    // exit;
+
+    $submittedValues = $event->getRecords();
+
+    // hack to check if commpref form is submitted
+    if (empty($submittedValues[0]['joins']['CommPrefGroup'])) {
+      return;
+    }
+
+    // process groups
+
+    // process emails
+
+    // process phone
+    \Civi\CommPref\BAO\Phone::processPhone($submittedValues[0]['fields']['id'], $submittedValues[0]['joins']['CommPrefPhone']);
+
   }
 
 }
