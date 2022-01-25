992daea03f333829448b67654b1beee1997ca096
Kurund Jalmi <kurundjalmi@thirdsectordesign.org>
2022-01-25 10:21:04 +0000

    fixed address saving and add fake apis to stopp errors from Afform

---
+++ Civi/Api4/Action/CommPrefEmail/Replace.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Civi\Api4\Action\CommPrefEmail;
+
+class Replace extends \Civi\Api4\Generic\BasicReplaceAction {
+
+  /**
+   * @inheritDoc
+   */
+  public function _run($result) {
+    // add fake replace as afform expects it else it will throw an error
+  }
+
+}
